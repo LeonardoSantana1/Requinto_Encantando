1c454c52a9384867c0fed97e3556a63b2d32bb1e
LeonardoSantana1 <leonardo-santana051@hotmail.com>
2025-04-23 02:58:59 -0300

    Fazendo slide de quem somos

---
+++ public/js/quemsomos.js
@@ -0,0 +1,16 @@
+export const Membros = [
+  {
+    nome: "Camila Andrade",
+    imagem: "img/camila.jpg",
+    descricao: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Maecenas nec eleifend magna, quis suscipit elit. Suspendisse ut ante felis. 
+    Phasellus augue est, venenatis nec diam eu, accumsan blandit felis.`,
+  },
+
+  {
+    nome: "João Ribeiro",
+    imagem: "img/joao.jpg",
+    descricao: `Cras cursus luctus lacinia. Nulla facilisi. 
+    Quisque massa erat, fermentum at semper porta, imperdiet vel nisl.`,
+  },
+];
--- public/js/sliderquemsomos.js
+++ public/js/sliderquemsomos.js
@@ -1,16 +1,49 @@
-export const Membros = [
-  {
-    nome: "Camila Andrade",
-    imagem: "img/camila.jpg",
-    descricao: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-  Maecenas nec eleifend magna, quis suscipit elit. Suspendisse ut ante felis. 
-  Phasellus augue est, venenatis nec diam eu, accumsan blandit felis.`,
-  },
+import { Membros } from "./quemsomos.js";
 
-  {
-    nome: "João Ribeiro",
-    imagem: "img/joao.jpg",
-    descricao: `Cras cursus luctus lacinia. Nulla facilisi. 
-  Quisque massa erat, fermentum at semper porta, imperdiet vel nisl.`,
-  },
-];
+(() => {
+  let currentSlide = 0;
+  const slidesContainer = document.querySelector(
+    ".SliderQuemSomos .QuemSomosSlides"
+  );
+  const totalSlides = Membros.length;
+
+  function renderSlides() {
+    slidesContainer.innerHTML = "";
+
+    Membros.forEach((membro, index) => {
+      const slide = document.createElement("div");
+      slide.classList.add("slide-qs");
+      if (index !== currentSlide) slide.style.display = "none";
+
+      slide.innerHTML = `
+         <div class="conteudo-qs">
+           <div class="bloco-qs foto-qs">
+             <img src="${membro.imagem}" alt="${membro.nome}">
+        </div>
+    <div class="bloco-qs texto-qs">
+      <h3>${membro.nome}</h3>
+      <p>${membro.descricao}</p>
+    </div>
+  </div>
+      `;
+
+      slidesContainer.appendChild(slide);
+    });
+  }
+
+  document
+    .querySelector(".SliderQuemSomos .next-qs")
+    .addEventListener("click", () => {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      renderSlides();
+    });
+
+  document
+    .querySelector(".SliderQuemSomos .prev-qs")
+    .addEventListener("click", () => {
+      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+      renderSlides();
+    });
+
+  renderSlides(); // inicia
+})();
