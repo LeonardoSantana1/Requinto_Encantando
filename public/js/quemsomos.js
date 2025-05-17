348b3c571091e86fc742e89a88151d1d4d9e7bfe
LeonardoSantana1 <leonardo-santana051@hotmail.com>
2025-05-17 14:40:15 -0300

    Ajeitando as fotos e o evento

--- public/js/quemsomos.js
+++ public/js/quemsomos.js
@@ -1,16 +1,25 @@
 export const Membros = [
   {
-    nome: "Camila Andrade",
-    imagem: "imagens/homem.jpg",
-    descricao: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Maecenas nec eleifend magna, quis suscipit elit. Suspendisse ut ante felis. 
-    Phasellus augue est, venenatis nec diam eu, accumsan blandit felis.`,
+    nome: "Maria Eduarda",
+    imagem: "imagens/Maria_SemBg.png",
+    descricao: `Gestora de Produção da Requinte Encantado. Responsável por garantir a melhor experiência gastronômica dos nossos clientes através do sabor encantador do doce de abóbora  gourmet`,
   },
 
   {
-    nome: "João Ribeiro",
-    imagem: "imagens/homen.jpg",
-    descricao: `Cras cursus luctus lacinia. Nulla facilisi. 
-    Quisque massa erat, fermentum at semper porta, imperdiet vel nisl.`,
+    nome: "Rudney",
+    imagem: "imagens/Rudney_SemBg.png",
+    descricao: `Gestor Financeiro da Requinte Encantado. Responsável por planejar, controlar, organizar e analisar as finanças de uma empresa, garantindo sua saúde financeira e sustentabilidade.`,
+  },
+
+  {
+    nome: "Jéssica Duraes",
+    imagem: "imagens/Jessica_SemBg.png",
+    descricao: `Gestora Comercial da Requinte Encantado.  Responsável por planejar, coordenar e supervisionar as atividades de vendas e relacionamento com clientes, garantindo a satisfação de cada cliente.`,
+  },
+
+  {
+    nome: "Evandro",
+    imagem: "imagens/Evandro_SemBg.png",
+    descricao: `Gestor de Marketing da Requinte Encantado. Responsável por planejar campanhas e estratégias de marketing, efetuar a análise de mercado e resultados, garantindo que chegue sempre o melhor conteúdo aos clientes.`,
   },
 ];
